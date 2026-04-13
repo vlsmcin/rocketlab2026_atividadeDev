7c9c49fcc3f9c2438a6a8e8065acd9a756fa222b
vlsmcin <viniciuslimamelo7@gmail.com>
2026-04-13 18:05:18 -0300

    feat: Create views for product page

--- backend/app/views/produtos.py
+++ backend/app/views/produtos.py
@@ -1,5 +1,20 @@
 from pydantic import BaseModel
 
+
+class AvaliacaoView(BaseModel):
+    id_avaliacao: str
+    avaliacao: int
+    nome_consumidor: str
+    titulo_comentario: str | None
+    comentario: str | None
+
+class VendedorView(BaseModel):
+    id_vendedor: str
+    nome_vendedor: str
+    preco_brl: float
+    cidade: str
+    estado: str
+
 class ProdutoListView(BaseModel):
     id_produto: str
     nome_produto: str
@@ -7,3 +22,17 @@ class ProdutoListView(BaseModel):
     url_imagem: str | None
     media_avaliacao: float | None
     quantidade_avaliacoes: int
+
+class ProdutoDetailView(BaseModel):
+    id_produto: str
+    nome_produto: str
+    categoria_produto: str
+    peso_produto_gramas: float | None
+    comprimento_centimetros: float | None
+    largura_centimetros: float | None
+    altura_centimetros: float | None
+    url_imagem: str | None
+    media_avaliacao: float | None
+    quantidade_avaliacoes: int
+    avaliacoes: list[AvaliacaoView] | None
+    vendedores: list[VendedorView] | None
